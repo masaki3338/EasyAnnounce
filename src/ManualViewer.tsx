8b6e15aeeef86463ed0818355b19d3a7df3cc7c2
masaki3338 <masakicarp@jcom.home.ne.jp>
2025-07-29 10:13:48 +0900

    Add files via upload

--- src/ManualViewer.tsx
+++ src/ManualViewer.tsx
@@ -1,15 +1,21 @@
 // ManualViewer.tsx
 import React from "react";
 import { Viewer, Worker } from "@react-pdf-viewer/core";
+import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/default-layout/lib/styles/index.css";
 
 const ManualViewer: React.FC = () => {
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+
   return (
-    <div className="w-full h-screen">
-        <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
-        <Viewer fileUrl="/manual.pdf" />
-        </Worker>
+    <div className="w-full h-full overflow-hidden">
+      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
+        <Viewer
+          fileUrl="/manual.pdf"
+          plugins={[defaultLayoutPluginInstance]}
+        />
+      </Worker>
     </div>
   );
 };
