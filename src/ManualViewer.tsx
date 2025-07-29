f8c3ce91c61fc3d7de688094f663f1ec97d923df
masaki3338 <masakicarp@jcom.home.ne.jp>
2025-07-29 11:02:22 +0900

    Add files via upload

--- src/ManualViewer.tsx
+++ src/ManualViewer.tsx
@@ -1,7 +1,5 @@
-import { Worker, Viewer } from "@react-pdf-viewer/core";
-import "@react-pdf-viewer/core/lib/styles/index.css";
-import "@react-pdf-viewer/default-layout/lib/styles/index.css";
-import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
+//import { Worker, Viewer } from "@react-pdf-viewer/core";
+//import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
 
 const ManualViewer = () => {
   const defaultLayoutPluginInstance = defaultLayoutPlugin();
