80a194fb6054b423f98509b4427f98a65a8ad525
masaki3338 <masakicarp@jcom.home.ne.jp>
2025-07-29 10:42:14 +0900

    Add files via upload

--- src/ManualViewer.tsx
+++ src/ManualViewer.tsx
@@ -1,4 +1,3 @@
-// ManualViewer.tsx
 import React from "react";
 import { Viewer, Worker } from "@react-pdf-viewer/core";
 import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
@@ -10,11 +9,9 @@ const ManualViewer: React.FC = () => {
 
   return (
     <div className="w-full h-full overflow-hidden">
-      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
-        <Viewer
-          fileUrl="/manual.pdf"
-          plugins={[defaultLayoutPluginInstance]}
-        />
+      {/* ✅ ローカルの worker を使う */}
+      <Worker workerUrl="/pdf.worker.min.js">
+        <Viewer fileUrl="/manual.pdf" plugins={[defaultLayoutPluginInstance]} />
       </Worker>
     </div>
   );
