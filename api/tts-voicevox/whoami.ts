4a7ca778f4195ad599b8502231d31d02c9d2b26f
masaki3338 <masakicarp@jcom.home.ne.jp>
2025-10-03 15:27:35 +0900

    Delete api/tts-voicevox/whoami.ts

--- api/tts-voicevox/whoami.ts
+++
@@ -1,6 +0,0 @@
-// api/tts-voicevox/whoami.ts
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default function handler(_req: VercelRequest, res: VercelResponse) {
-  res.status(200).json({ ok: true, by: 'vercel-node-function', at: '/api/tts-voicevox/whoami' });
-}
