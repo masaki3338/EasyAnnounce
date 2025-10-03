dbe5fa861fcd1ca0b4e24d10194df03d5e247219
masaki3338 <masakicarp@jcom.home.ne.jp>
2025-10-03 16:13:57 +0900

    Add files via upload

--- api/ping.js
+++ api/ping.js
@@ -1,3 +1,4 @@
 // api/ping.js
-module.exports = (req, res) => res.status(200).json({ ok: true, now: Date.now() });
-module.exports.config = { runtime: 'nodejs' };
+module.exports = (req, res) => {
+  res.status(200).json({ ok: true, where: '/api/ping' });
+};
