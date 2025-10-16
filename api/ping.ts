944436c2669a510d08161f13901fd590c7777e5f
masaki3338 <masakicarp@jcom.home.ne.jp>
2025-10-16 17:02:09 +0900

    Delete api directory

--- api/ping.ts
+++
@@ -1,6 +0,0 @@
-// api/ping.ts
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-export default async function handler(_req: VercelRequest, res: VercelResponse) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.status(200).json({ ok: true, where: "/api/ping" });
-}
